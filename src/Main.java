267f69e8df634da1a8561fa4a1b2961d2e3dd5b7
kurmanbek89 <kumakary2@gmail.com>
2022-11-06 10:36:37 +0300

    f

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        FileWriter writer = new FileWriter("file.txt");
+        writer.write("A a\n" + "B b\n" + "C c\n" + "D d\n" + "E e\n" + "F f\n" + "G g\n" + "H h\n" + "I i\n" + "J j\n" + "K k\n"
+                + "L l\n" + "M m\n" + "N n\n" + "O o\n" + "P p\n" + "Q q\n" + "R r\n" + "S s\n" + "T t\n" + "U u\n"
+                + "V v\n" + "W w\n" + "X x\n" + "Y y\n" + "Z z\n");
+        for (int i = 0; i <= 9; i++) {
+            writer.write(i + "\n");
+        }
+        writer.close();
+
+        FileReader reader = new FileReader("file.txt");
+        Scanner scanner = new Scanner(reader);
+        int count = 0;
+        while (scanner.hasNextLine()) {
+            count++;
+            System.out.println(count + ": " + scanner.nextLine());
+        }
     }
 }
